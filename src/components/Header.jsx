f17472d4033bf26d597580eab5cd3a9fbecd9119
Promethesus <jeremyandrews707@gmail.com>
2022-06-23 08:57:14 -0700

    Minor updates to colors in header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,25 +1,26 @@
 import React from 'react'
 import { Link } from "react-router-dom";
 import styles from "./Header.module.scss"
-import {BiMenuAltRight} from 'react-icons/bi'
-import {AiOutlineClose} from 'react-icons/ai'
+import { BiMenuAltRight } from 'react-icons/bi'
+import { AiOutlineClose } from 'react-icons/ai'
 
 function Header() {
   return (
     <header className={styles.header_container}>
       <div className={styles.header_content}>
-          <h2 className={styles.header_content_logo}>BabyData</h2>
+        <h2 className={styles.header_content_logo}>BabyData</h2>
         <nav className={styles.header_content_nav}>
           <ul>
             <li><Link to={"/histograms"}>Histograms</Link></li>
             <li><Link to={"/"}>PageTwo</Link></li>
             <li><Link to={"/"}>PageThree</Link></li>
           </ul>
-          <button className={styles.signUp}>SignUp</button>
-          <button className={styles.login}>Login</button>
+          <ul className={styles.header_content_nav_button}>
+            <button className={styles.login}>Login</button>
+          </ul>
         </nav>
         <div className={styles.header_content_toggle}>
-          <BiMenuAltRight/>
+          <BiMenuAltRight />
         </div>
       </div>
     </header>
